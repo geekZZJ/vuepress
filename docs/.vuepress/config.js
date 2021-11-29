a098973bea4bb2a3d7e8840ab054f8957b5213c7
zzj <2461927976@qq.com>
2021-11-29 23:40:00 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: zzj
+ * @Date: 2021-01-04 16:10:03
+ * @LastEditors: zzj
+ * @LastEditTime: 2021-03-08 18:17:04
+ * @Description:
+ */
+module.exports = {
+  title: "zzj的小站",
+  description: "我的小站",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/guide/" },
+      { text: "github", link: "https://github.com/geekZZJ" },
+    ],
+  },
+};
---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: zzj
+ * @Date: 2021-01-04 16:10:13
+ * @LastEditors: zzj
+ * @LastEditTime: 2021-01-04 16:10:15
+ * @Description:
+ */
